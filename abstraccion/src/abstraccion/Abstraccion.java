f9239a7df74993e3a00a8dda9dec2b6839414399
T-102 <T-102@PC261>
2017-09-18 19:43:27 -0500

    ya casi acabo, mi primer clase

---
+++ abstraccion/src/abstraccion/Abstraccion.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package abstraccion;
+
+/**
+ *
+ * @author T-102
+ */
+public class Abstraccion {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    Usuario u;
+    String resultado;
+    
+    public String calcular()}
+float mivalor=u.peso/(u.altura+u.altura);
+resultado=
+}

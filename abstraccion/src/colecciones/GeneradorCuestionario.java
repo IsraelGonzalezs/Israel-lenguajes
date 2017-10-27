109cb6f73ae42635d2b7706064497fa14e74711b
T-102 <T-102@PC261>
2017-10-26 19:46:37 -0500

    hola que tal buenas noches

---
+++ abstraccion/src/colecciones/GeneradorCuestionario.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package colecciones;
+
+/**
+ *
+ * @author T-102
+ */
+public class GeneradorCuestionario {
+    public ArrayList<Pregunta>/*Generico a Pregunta*/generar(){
+    //Primero generamos el arreglo que contendra las preguntas
+    ArrayList
+}

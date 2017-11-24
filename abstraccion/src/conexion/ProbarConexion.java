d7cc32e773a62366c203ca482c27fae8a2eecf69
T-102 <T-102@PC261>
2017-11-23 19:51:08 -0600

    my secret sql1

---
+++ abstraccion/src/conexion/ProbarConexion.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package conexion;
+
+import java,sql.*;
+public class ProbarConexion {
+    public static void main(String[] args) {
+        try{
+    }
+    Connection con=   Conexion.conectarse("root", "");
+            System.out.println("Te conectaste!!");
+            //Aqui vienen quieres de mysql
+            }catch(ClassNotFundException e){
+    
+            }catch(SQLException e){
+    
+            }finally{
+                con.
+    
+}
+    
+}
